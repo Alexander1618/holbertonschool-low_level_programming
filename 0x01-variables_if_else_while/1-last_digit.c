19aa6c8f5c8196d3f62bba49e3463f2abb8cf304
Alexander1618 <aligator1618@hotmail.com>
2021-02-04 15:56:40 -0500

    Test 0 and 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+*generate a random number af said if negative or positive
+*/
+int main(void)
+{
+	int n;
+	unsigned short lastNumber;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastNumber = (n % 10);
+	if(lastNumber == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastNumber);
+	else if (lastNumber > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", 
+n, lastNumber);
+	else
+		printf("Last digit of %d is %d and is less than 6\n", 
+n, lastNumber);
+	return (0);
+}

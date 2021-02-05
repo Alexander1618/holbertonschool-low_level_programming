235759af96084eedd4896c91be20ddb90d6fa6a9
Alexander1618 <aligator1618@hotmail.com>
2021-02-04 22:15:27 -0500

    Test Negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
-*generate a random number af said if negative or positive
-*/
-int main(void)
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */int main(void)
 {
 	int n;
 	
@@ -13,10 +15,10 @@ int main(void)
 	
 	if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
-	else
+	else if (n < 0)
 		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
 
 	return (0);
 }

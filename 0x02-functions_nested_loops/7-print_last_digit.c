f9408cb925143cd350d1822d0f6d125dc3cfe85e
Alexander1618 <aligator1618@hotmail.com>
2021-02-11 17:49:19 -0500

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_last_digit - Write a function that prints the last digit of a number.
+ * @r: Entero
+ * Return: 0
  */
 int print_last_digit(int r)
 {
 	r = abs(r);
-	r = r % 10;
+        r = r % 10;
 	_putchar(r + '0');
 	return (0);
 }
